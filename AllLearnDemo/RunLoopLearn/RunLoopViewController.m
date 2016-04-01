09d0c24eadab641fe564b6f0d1d3be638dfac170
icemilk00 <442377973@qq.com>
2016-04-01 17:42:46 +0800

    runloop code

--- AllLearnDemo/RunLoopLearn/RunLoopViewController.m
+++ AllLearnDemo/RunLoopLearn/RunLoopViewController.m
@@ -20,13 +20,35 @@
     self.view.backgroundColor = [UIColor whiteColor];
     
     [self seeCurrentLoop];      //查看runloop结构
-
+    [self makeMySource];
 }
 
 -(void)seeCurrentLoop
 {
     NSRunLoop *currentLoop = [NSRunLoop currentRunLoop];
     NSLog(@"currentLoop = %@", currentLoop);
+}
+
+-(void)makeMySource
+{
+    dispatch_source_t source = dispatch_source_create(DISPATCH_SOURCE_TYPE_DATA_ADD, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+    dispatch_source_set_event_handler(source, ^{
+//        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:@"Source Event" delegate:self cancelButtonTitle:@"ok" otherButtonTitles:nil, nil];
+//        [alert show];
+        NSLog(@"1111");
+    });
+    
+    dispatch_resume(source);
+    
+    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0));
+    dispatch_source_set_timer(timer, DISPATCH_TIME_NOW, 1ull * NSEC_PER_SEC, 0);
+    dispatch_source_set_event_handler(timer, ^{
+        dispatch_source_merge_data(source, 1);
+    });
+    dispatch_resume(timer);
+    
+//    dispatch_main();
+    
 }
 
 
