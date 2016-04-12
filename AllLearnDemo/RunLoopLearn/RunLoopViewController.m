a72ae1c8280b8636075784bc53988ee7d429a480
icemilk00 <442377973@qq.com>
2016-04-12 20:13:22 +0800

    添加测试代码

--- AllLearnDemo/RunLoopLearn/RunLoopViewController.m
+++ AllLearnDemo/RunLoopLearn/RunLoopViewController.m
@@ -8,13 +8,15 @@
 
 #import "RunLoopViewController.h"
 
+
 @interface RunLoopViewController () <UIScrollViewDelegate>
 {
-    CFRunLoopObserverRef observer;
-    NSTimer *repeatTimer;
+    CFRunLoopObserverRef observer;  //runloop观察者
     
-    NSRunLoop *secondRunLoop;
-    CFRunLoopSourceRef secondSource;
+    NSThread *secondThread;
+    NSTimer *repeatTimer;           //测试定时器
+    NSRunLoop *secondRunLoop;       //第二线程的runloop
+    CFRunLoopSourceRef secondSource;//第二线程的source
 }
 @end
 
@@ -25,10 +27,10 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
-    [self seeCurrentLoop];      //查看runloop结构
-//    [self addRunLoopObserveWithRunLoop:[NSRunLoop currentRunLoop]];   //runloop观察者测试代码
-    [self runRunLoop];          //runloop运行代码
-    
+    [self seeCurrentLoop];                  //查看runloop结构
+//    [self addRunLoopObserve];               //runloop观察者测试代码
+    [self runRunLoop];                      //第二线程runloop运行代码
+
     
 }
 
@@ -41,8 +43,9 @@
 /*
  *  在当前线程创建一个runLoop观察者，并观察runloop的所有活动状态，看runloop运行顺序及一些源的运行
  */
--(void)addRunLoopObserveWithRunLoop:(NSRunLoop *)currentRunLoop
+-(void)addRunLoopObserve
 {
+    NSRunLoop *currentRunLoop = [NSRunLoop currentRunLoop];
     CFRunLoopObserverCallBack runLoopObserverCallBack = myRunLoopObserverCallBack;
     CFRunLoopObserverContext observerContext = {0, (__bridge void *)(self), NULL, NULL, NULL};
     
@@ -54,33 +57,43 @@
     }
 }
 
+void myRunLoopObserverCallBack(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
+{
+    NSLog(@"myRunLoopObserverCallBack's status = %lu", activity);
+}
+
 /*
- *  启动一个runLoop并保持，主要是在非主线程
+ * 主线程与第二线程通信，通过定时器发送消息，需要第二线程保持不退出
+ * 在第二启动一个runLoop并添加source源，添加源主要为保持线程不退出，以响应主线程发送的消息来响应
+ *
  */
 -(void)runRunLoop
 {
-    NSThread *secondThread = [[NSThread alloc] initWithTarget:self selector:@selector(secondThreadAction) object:nil];
+    secondThread = [[NSThread alloc] initWithTarget:self selector:@selector(secondThreadAction) object:nil];
     [secondThread start];
     
-    repeatTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(repeatTest:) userInfo:secondThread repeats:YES];
+    repeatTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(repeatTest:) userInfo:nil repeats:YES];
     
 }
 
 -(void)secondThreadAction
 {
+    [self addRunLoopObserve];
+    
+    
     NSLog(@"CurrentThread = %@", [NSThread currentThread]);
     @autoreleasepool {
         secondRunLoop = [NSRunLoop currentRunLoop];
         CFRunLoopSourceContext context = {0, (__bridge void *)(self), NULL, NULL, NULL};
         secondSource =  CFRunLoopSourceCreate(kCFAllocatorDefault, 0, &context);
-        CFRunLoopAddSource([secondRunLoop getCFRunLoop], secondSource, kCFRunLoopDefaultMode);
+        CFRunLoopAddSource([secondRunLoop getCFRunLoop], secondSource, kCFRunLoopCommonModes);
         [secondRunLoop run];
     }
 }
 
 -(void)repeatTest:(NSTimer *)timer
 {
-    [self performSelector:@selector(testAction) onThread:timer.userInfo withObject:nil waitUntilDone:nil modes:@[NSDefaultRunLoopMode]];
+    [self performSelector:@selector(testAction) onThread:secondThread withObject:nil waitUntilDone:nil modes:@[NSRunLoopCommonModes]];
 }
 
 -(void)testAction
@@ -88,26 +101,37 @@
     NSLog(@"This is test");
 }
 
-void myRunLoopObserverCallBack(CFRunLoopObserverRef observer, CFRunLoopActivity activity, void *info)
-{
-    NSLog(@"myRunLoopObserverCallBack's status = %lu", activity);
-}
+
 
 -(void)viewWillDisappear:(BOOL)animated
 {
     if (repeatTimer && repeatTimer.isValid) {
-        [repeatTimer invalidate];
+        [repeatTimer invalidate], repeatTimer = nil;
     }
-    
-    CFRunLoopRemoveSource([secondRunLoop getCFRunLoop], secondSource, kCFRunLoopDefaultMode);
-    CFRelease(secondSource);
-    //CFRunLoopStop() 是需要把 runloop中的源清掉的，否则在stop之后会重新wake up起runloop，所以主线程的runloop调用这个方法没有用，系统会把一些系统的源重新加进去并wake up
-    CFRunLoopStop([secondRunLoop getCFRunLoop]);
+   
+    if (secondRunLoop && secondSource) {
+        
+        
+        NSLog(@"secondRunloop 1 = %@", secondRunLoop);
+        
+        CFRunLoopRemoveSource([secondRunLoop getCFRunLoop], secondSource, kCFRunLoopCommonModes);
+        NSLog(@"secondRunloop 2 = %@", secondRunLoop);
+        //    CFRunLoopStop() 是需要把 runloop中的源清掉的，否则在stop之后会重新wake up起runloop，所以主线程的runloop调用这个方法没有用，系统会把一些系统的源重新加进去并wake up
+        CFRunLoopStop([secondRunLoop getCFRunLoop]);
+
+    }
 }
+
 
 -(void)dealloc
 {
-    CFRunLoopRemoveObserver([[NSRunLoop currentRunLoop] getCFRunLoop], observer, kCFRunLoopDefaultMode);
+    if (observer) {
+        CFRunLoopRemoveObserver([[NSRunLoop currentRunLoop] getCFRunLoop], observer, kCFRunLoopCommonModes);
+        CFRelease(observer);
+    }
+    
+    if(secondSource) CFRelease(secondSource);
+    
 }
 
 - (void)didReceiveMemoryWarning {
